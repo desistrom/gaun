2771c7d8b9cd6c9ad7e855b2e71fe94801f742ff
desistrom0 <desistrom0@gmail.com>
2018-10-04 20:54:11 +0700

    idren

--- system/core/Security.php
+++ system/core/Security.php
@@ -575,8 +575,8 @@ class CI_Security {
 		}
 
 		// Unfortunately, none of the following PRNGs is guaranteed to exist ...
-		// if (defined('MCRYPT_DEV_URANDOM') && ($output = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM)) !== FALSE)
-		if (defined('MCRYPT_DEV_URANDOM') && ($output = random_bytes($length)) !== FALSE)
+		if (defined('MCRYPT_DEV_URANDOM') && ($output = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM)) !== FALSE)
+		// if (defined('MCRYPT_DEV_URANDOM') && ($output = random_bytes($length)) !== FALSE)
 		{
 			return $output;
 		}

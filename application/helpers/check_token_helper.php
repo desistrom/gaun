3ead816dabc3499f09795de0df86d650ef113f70
desistrom <deistrom0@gmail.com>
2018-10-11 16:23:53 +0700

    jwt

--- application/helpers/check_token_helper.php
+++ application/helpers/check_token_helper.php
@@ -3,7 +3,7 @@
  if (!function_exists('check_token')) {
  	function check_token(){
  		$CI =& get_instance();
-        $CI->load->library("JWT");
+        $CI->load->library("jwt");
         // $CONSUMER_KEY = 'ingDLMRuGe9UKHRNjs7cYckS2yul4lc3';
         $CONSUMER_SECRET = 'junaedi19981101';
         // $CONSUMER_TTL = 86400;
